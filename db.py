39e9db9184e1a3e6ab9b3ddd013714706cc627f1
Alexey Trubin <alxjnk@gmail.com>
2020-03-11 19:27:16 +0300

    new db.py

--- db.py
+++ db.py
@@ -3,11 +3,24 @@ import pymysql
 con = pymysql.connect('localhost', 'agronova_parse', 
     '3c91250b!!!', 'agronova_bs')
  
-with con:
-    
-    cur = con.cursor()
-    cur.execute("SELECT VERSION()")
- 
-    version = cur.fetchone()
-    
-    print("Database version: {}".format(version[0]))
+try:
+    with con.cursor() as cursor:
+        # Create a new record
+        # sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        sql = "SELECT * from oc_product"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    # con.commit()
+
+    # with con.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+    #     cursor.execute(sql, ('webmaster@python.org',))
+    #     result = cursor.fetchone()
+    #     print(result)
+finally:
+    con.close()
