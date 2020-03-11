55616bd05a9939a253e77f4db34f8eb5f244e351
Alexey Trubin <alxjnk@gmail.com>
2020-03-11 19:16:53 +0300

    new db

--- db.py
+++ db.py
@@ -1,17 +1,13 @@
-import psycopg2
-
-hostname = 'balarama.db.elephantsql.com'
-username = 'nmnwuopl' # the username when you create the database
-password = 'O6eGuGRaw1iqOeU4xecihU0kESQkRhUo' #change to your password
-database = 'nmnwuopl'
-
-def queryQuotes( conn ) :
-    cur = conn.cursor()
-    cur.execute( """select * from amako""" )
-    rows = cur.fetchall()
-    for row in rows :
-        print (row[5])
-
-conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
-queryQuotes( conn )
-conn.close()
+import pymysql
+ 
+con = pymysql.connect('localhost', 'agronova_parse', 
+    '3c91250b!!!', 'agronova_bs')
+ 
+with con:
+    
+    cur = con.cursor()
+    cur.execute("SELECT VERSION()")
+ 
+    version = cur.fetchone()
+    
+    print("Database version: {}".format(version[0]))
