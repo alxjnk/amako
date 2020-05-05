59d4b7e646d5637b84c297366572df9a11da5c71
Alexey Trubin <alxjnk@gmail.com>
2020-05-05 16:43:38 +0300

    changed output

--- amakoparts/spiders/amako.py
+++ amakoparts/spiders/amako.py
@@ -20,10 +20,13 @@ def quantity_serializer(value):
 class Product(scrapy.Item):
     DT_RowId = scrapy.Field()
     title = scrapy.Field()
+    metatitle = scrapy.Field()
+    descriptions = scrapy.Field()
     partid = scrapy.Field()
+    sku = scrapy.Field()
     manufacturerid = scrapy.Field()
     manufacturer = scrapy.Field()
-    quantity = scrapy.Field(serializer=quantity_serializer)
+    # quantity = scrapy.Field(serializer=quantity_serializer)
     price = scrapy.Field(serializer=float_serializer)
     actionprice = scrapy.Field()
     rep = scrapy.Field()
@@ -41,16 +44,25 @@ class MySpider(scrapy.Spider):
 
     def parse(self, response):
         ln = 999999
-        hrefs = ('response url', response.selector.xpath(
-            '//*[@id="List"]/li/a/@href').getall())
-        titles = ('response url', response.selector.xpath(
-            '//*[@id="List"]/li/a/@title').getall())
-        man_imgs = ('response url', response.selector.xpath(
-            '//*[@id="List"]/li/a/img/@src').getall())
+        hrefs = response.selector.xpath(
+            '//*[@id="List"]/li/a/@href').getall()
+        titles = response.selector.xpath(
+            '//*[@id="List"]/li/a/@title').getall()
+        man_imgs = response.selector.xpath(
+            '//*[@id="List"]/li/a/img/@src').getall()
 
         for href, title, imgs in zip(hrefs, titles, man_imgs):
-            for h, t, i in zip(href, title, imgs):
-                yield scrapy.http.Request('https://parts.amaco.ua/services/search/data.html?draw=1&columns%5B0%5D%5Bdata%5D=title&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=manufacturer&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=partid&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=quantity&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=price&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length={1}&search%5Bvalue%5D=&search%5Bregex%5D=false&lang=1&manufacturer={0}&_=1577614837927'.format(h[2:-1], ln), callback=self.parse_manufacturer, meta={'manufacturer': t, 'man_img': i}, dont_filter=True)
+            # print('href', len(hrefs))
+            # print('title', len(titles))
+            # print('man_imgs', len(man_imgs))
+            # for h, t, i in zip(href, title, imgs):
+                # if (i == 'r'):
+                #     print('imgs', imgs)
+                #     print('IMGS', i)
+                #     print('TITLE', t)
+                #     print('man-imgs', man_imgs)
+                #     break
+            yield scrapy.http.Request('https://parts.amaco.ua/services/search/data.html?draw=1&columns%5B0%5D%5Bdata%5D=title&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=manufacturer&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=partid&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=quantity&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=price&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length={1}&search%5Bvalue%5D=&search%5Bregex%5D=false&lang=1&manufacturer={0}&_=1577614837927'.format(href[2:-1], ln), callback=self.parse_manufacturer, meta={'manufacturer': title, 'man_img': imgs}, dont_filter=True)
                 # break
 # https://parts.amaco.ua/ru/part/4ba5315f-f513-11e3-b5c5-00155d012119
 
@@ -61,6 +73,7 @@ class MySpider(scrapy.Spider):
         man_img = response.meta.get('man_img')
 
         for item in jsonresponse['data']:
+            del item['quantity']
             product = Product(item)
             product['manufacturer'] = manufacturer
             product['manufacturer_img'] = 'https://parts.amaco.ua/ru' + man_img
@@ -74,15 +87,18 @@ class MySpider(scrapy.Spider):
         for row in response.selector.xpath('//*[@id="Replacements"]/table/tbody/tr'):
             row = row.xpath('td/a/text()').extract()
             if len(row) == 3:
-                replacements.append(row[1] + ' ' + row[0])
+                replacements.append(row[1]) # + ' ' + row[0])
             if len(row) == 4:
-                replacements.append(row[2] + ' ' + row[0])
+                replacements.append(row[2]) # + ' ' + row[0])
         if img == None:
             img = 'no image'
         else:
             img = 'https://parts.amaco.ua/ru' + img
-        product['quantity'] = str(product['quantity'])
+        # product['quantity'] = str(product['quantity'])
         product['img_link'] = img
         product['replacements'] = replacements
         product['title'] = product['partid'] + ' ' + product['title']
+        product['metatitle'] = product['partid'] + ' ' + product['title']
+        product['descriptions'] = product['partid'] + ' ' + product['title']
+        product['sku'] = product['partid']
         yield product
